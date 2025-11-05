36707166ae65e69ab18e5aedc93aab2425c250b0
Kezypie <wwwkrisna502@gmail.com>
2025-11-05 20:50:43 +0700

    menambahkan input nilai dari 5 matkul

---
+++ Nilai.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Nilai {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double strukturData,PemrogramanBO,BasisData,algoritmaPemrograman,RekayasaPerangkatLunak;
+
+
+        System.out.print("Masukkan Nilai Struktur Data: ");
+        strukturData = sc.nextDouble();
+        System.out.print("Masukkan Nilai Pemrograman Berorientasi Objek: ");
+        PemrogramanBO = sc.nextDouble();
+        System.out.print("Masukkan Nilai Basis Data: ");
+        BasisData = sc.nextDouble();
+        System.out.print("Masukkan Nilai Algoritma dan Pemrograman: ");
+        algoritmaPemrograman = sc.nextDouble();
+        System.out.print("Masukkan Nilai Rekayasa Perangkat: ");
+        RekayasaPerangkatLunak = sc.nextDouble(); 
+    }
+}
