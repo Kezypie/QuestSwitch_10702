198f26fc1aafde75efbb4cc5fd39cc2a320b7ead
Kezypie <wwwkrisna502@gmail.com>
2025-11-05 20:48:49 +0700

    declare variable

--- nugas.java
+++ nugas.java
@@ -2,5 +2,9 @@ import java.util.Scanner;
 
 public class nugas {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double strukturData,PemrogramanBO,BasisData,algoritmaPemrograman,RekayasaPerangkatLunak;
+
     }
 }
